25b98b5327a8decfa6668e3a7216b52275dd5e86
Ioan Hambasan <Ioan.Hambasan@iquestgroup.com>
2019-04-07 18:47:22 +0300

    add mutex so that the application can be started only once per machine

--- Desktop/Agent/Program.cs
+++ Desktop/Agent/Program.cs
@@ -1,20 +1,27 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Agent
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using (var mutex = new Mutex(false, "isbsoftware.com CallCenter"))
+            {
+                bool isAnotherInstanceOpen = !mutex.WaitOne(TimeSpan.Zero);
+                if (isAnotherInstanceOpen)
+                {
+                    Console.WriteLine(@"Only one instance of this app is allowed.");
+                    return;
+                }
 
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
