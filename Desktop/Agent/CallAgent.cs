25c174ee3e4657e6ccb778778c6f99e9313a912c
isbsoftware <isbsoftware@outlook.com>
2019-04-13 15:46:17 +0300

    Add Libs folder and point reference to SIP dll.

---
+++ Desktop/Agent/CallAgent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+using DataAccess.Services;
+
+namespace Agent
+{
+    public partial class CallAgent : Form
+    {
+        public CallAgent()
+        {
+            InitializeComponent();
+        }
+
+        private void lblLoggedSince_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StatusService asd = new StatusService();
+
+            asd.GetAll();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Desktop/Agent/Program.cs
+++ Desktop/Agent/Program.cs
@@ -20,7 +20,7 @@ namespace Agent
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
+                Application.Run(new CallAgent());
             }
         }
     }
