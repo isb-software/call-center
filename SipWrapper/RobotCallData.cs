40927030333ab9aee03d96c9d4c67553192eae49
isbsoftware <isbsoftware@outlook.com>
2019-05-03 17:46:09 +0300

    Add robot stuff.

---
+++ SipWrapper/RobotCallData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SipWrapper
+{
+    public class RobotCallData
+    {
+        public string Status { get; internal set; }
+        public bool Successful { get; internal set; }
+        public TimeSpan CallDuration { get; internal set; }
+    }
+}
